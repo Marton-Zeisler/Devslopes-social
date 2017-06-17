ff61e28a30804298b6b3e74b33842f9202f7f5b1
Marton Zeisler <marton@martons-mac-mini.local>
2017-06-17 22:56:40 +0200

    Initial Commit

---
+++ devslopes-social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  devslopes-social
+//
+//  Created by Marton Zeisler on 2017. 06. 17..
+//  Copyright © 2017. scapp. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
