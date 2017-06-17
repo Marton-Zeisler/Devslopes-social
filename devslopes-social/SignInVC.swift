18f8f4d86a8fdee317fe760c5f6a0ad22dd4e534
Marton Zeisler <marton@martons-mac-mini.local>
2017-06-18 01:43:56 +0200

    Facebook sign in

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -21,6 +24,47 @@ class SignInVC: UIViewController {
         
     
     }
+    
+    
+    
+    @IBAction func facebookBtnPressed(_ sender: Any) {  // Facebook authorization
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil{
+                print("Marton: Unable to authenticate with Facebook - \(String(describing: error))")
+            }else if result?.isCancelled == true{
+                print("Marton: User cancalled Facebook authentication")
+            }else{
+                print("Marton: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString )
+                self.firebaseAuth(credential)
+            }
+        
+            
+        }
+        
+    } // end of func
+    
+    func firebaseAuth(_ credential: AuthCredential) {   // // Firebase authorization
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Marton: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("Marton: Successfully authenticated with Firebase")
+                
+                
+//                if let user = user {
+//                    let userData = ["provider": credential.provider]
+//                    self.completeSignIn(id: user.uid, userData: userData)
+//                }
+                
+                
+            }
+        })
+    } // end of func
+    
     
     
     
